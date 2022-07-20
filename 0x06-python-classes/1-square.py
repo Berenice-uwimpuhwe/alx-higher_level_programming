f7c4697551cdbf89146a3c24057a1a7883cfbba4
Berenice-uwimpuhwe <uberenice16@gmail.com>
2022-07-20 13:13:07 -0700

    Task 1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defining a class Square"""
+
+
+class Square:
+    """Square representation"""
+
+    def __init__(self, size):
+        """"Initializing a new Square
+
+        Args:
+            size (int): size of the new Square
+        """
+        self.__size = size
